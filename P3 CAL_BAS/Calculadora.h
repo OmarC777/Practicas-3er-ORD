2bc99eec4463fef5d83d82b9a8302ec1915ff078
OmarC777 <omarcruzortega07@gmail.com>
2026-06-17 16:39:13 -0600

    Calculadora Basica (Guardando en archivo)

---
+++ P3 CAL_BAS/Calculadora.h
@@ -0,0 +1,17 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+class Calculadora
+{
+	public:
+		void Resta();
+		void Suma();
+		void Multiplicacion();
+		void Division();
+		void Operaciones();
+		Calculadora();
+		~Calculadora();
+	protected:
+};
+
+#endif
